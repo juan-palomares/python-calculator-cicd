60b7004598bfa4296649cc5c09275a8c8950ad24
Juan Palomares <juan.palomares@ik.me>
2026-02-19 00:14:35 -0800

    fixed more errors

--- test_calculator.py
+++ test_calculator.py
@@ -14,13 +14,11 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(0, 0), 0)
 
-
     def test_subtract(self):
         """Test subtraction function"""
         self.assertEqual(subtract(5, 3), 2)
         self.assertEqual(subtract(9, 10), -1)
         self.assertEqual(subtract(0, 0), 0)
-
 
     def test_multiply(self):
         """Test multiplication function"""
@@ -28,26 +26,22 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(multiply(1, -3), -3)
         self.assertEqual(multiply(0, 10), 0)
 
-
     def test_divide(self):
         """Test division function"""
         self.assertEqual(divide(10, 2), 5)
         self.assertEqual(divide(9, 3), 3)
         self.assertEqual(divide(-8, 4), -2)
         
-
     def test_divide_by_zero(self):
         """Test dividing by zero error function"""
         with self.assertRaises(ValueError):
             divide(10,0)
-
 
     def test_power(self):
         """Test power function"""
         self.assertEqual(power(2, 3), 8)
         self.assertEqual(power(5, 2), 25)
         self.assertEqual(power(100, 0), 1)
-
 
 if __name__ == '__main__':
     unittest.main()
